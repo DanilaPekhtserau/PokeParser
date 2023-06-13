05bd36bd0a143f44b8bc7dd647e8985ce694786f
Danila <danila.pekhtserau@gmail.com>
2023-06-13 20:28:47 +0300

    remove debug print

--- pokemon_parser.py
+++ pokemon_parser.py
@@ -1,5 +1,4 @@
 def parse_pokemon_data(pokemon_item, id_counter):
-    print(pokemon_item, id_counter)
     name = pokemon_item.find('h2', class_='woocommerce-loop-product__title').text
     price = pokemon_item.find('span', class_='woocommerce-Price-amount amount').text
     sku = pokemon_item.find('a', class_='add_to_cart_button').get('data-product_sku', '')
